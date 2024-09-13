181c9b227ecbe09b5d64faa344bc5a96d0ee83de
AnonymousAcid <anonymousacid14@gmail.com>
2024-09-13 12:57:49 -0700

    Arm struct + custom mb joint addition
    This avoids the crash that happens when using bevy_rapier's MultibodyJoint component.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use bevy::{app::{App, Startup}, math::Vec3, prelude::{default, Camera3dBundle, Commands, Transform, TransformBundle}, DefaultPlugins};
+use bevy::{app::{App, PostUpdate, Startup}, math::Vec3, prelude::{default, Added, Camera3dBundle, Commands, Component, Entity, IntoSystemConfigs, Parent, Query, ResMut, Transform, TransformBundle}, DefaultPlugins};
 use bevy_flycam::{FlyCam, MovementSettings, NoCameraPlayerPlugin};
-use bevy_rapier3d::{na::coordinates::X, plugin::RapierPhysicsPlugin, prelude::{Collider, CollisionGroups, FixedJoint, GenericJoint, GenericJointBuilder, Group, ImpulseJoint, JointAxesMask, JointAxis, MultibodyJoint, RevoluteJointBuilder, RigidBody, SphericalJointBuilder}, render::RapierDebugRenderPlugin};
+use bevy_rapier3d::{plugin::{systems::init_joints, RapierContext, RapierPhysicsPlugin}, prelude::{Collider, CollisionGroups, FixedJoint, GenericJoint, GenericJointBuilder, Group, ImpulseJoint, JointAxesMask, JointAxis, MultibodyJoint, RapierMultibodyJointHandle, RevoluteJointBuilder, RigidBody, SphericalJointBuilder, TypedJoint}, render::RapierDebugRenderPlugin};
 
 fn main() {
     let mut app = App::new();
@@ -12,7 +12,8 @@ fn main() {
     ));
 
     app
-        .add_systems(Startup, startup);
+        .add_systems(Startup, startup)
+        .add_systems(PostUpdate, chain_arm_joints.after(init_joints));
 
     //flycam config
     app.world_mut().resource_mut::<MovementSettings>()
@@ -80,28 +81,87 @@ fn startup(
         let shld_x_j = RevoluteJointBuilder::new(Vec3::X)
             .local_anchor1(Vec3::Y * (torso_len/2.))
             .limits([-150f32.to_radians(), 60f32.to_radians()])
-            .motor(0., motor_vel, 1., 0.0)
-            .motor_max_force(20.);
+            .motor(0., motor_vel, 1., 0.0);
         let shld_y_j = RevoluteJointBuilder::new(Vec3::Y)
             .limits([-90f32.to_radians(), 90f32.to_radians()])
-            .motor(0., motor_vel, 1., 0.0)
-            .motor_max_force(20.);
+            .motor(0., motor_vel, 1., 0.0);
         let shld_z_j = RevoluteJointBuilder::new(Vec3::Z)
             .local_anchor2(Vec3::Y * (arm_upper_len/2.))
             .limits([0., 180f32.to_radians()])
-            .motor(90f32.to_radians(), motor_vel, 1., 0.0)
-            .motor_max_force(20.);
+            .motor(90f32.to_radians(), motor_vel, 1., 0.0);
         let mut no_collision_j = FixedJoint::default();
             no_collision_j.data.raw = default();
             no_collision_j.set_contacts_enabled(false);
 
 
-        let shld_x = commands.spawn((RigidBody::Dynamic, MultibodyJoint::new(torso, shld_x_j.into()))).id();
-        let shld_y = commands.spawn((RigidBody::Dynamic, MultibodyJoint::new(shld_x, shld_y_j.into()))).id();
+        let shld_x = commands.spawn(RigidBody::Dynamic).id();
+        let shld_y = commands.spawn(RigidBody::Dynamic).id();
         let arm_r_upper = make_part!(arm_upper_len)
-            .insert(MultibodyJoint::new(shld_y, shld_z_j.into()))
             .insert(ImpulseJoint::new(torso, no_collision_j))
             .id();
 
+        let elb_x_j = RevoluteJointBuilder::new(Vec3::X)
+            .local_anchor1(Vec3::Y * (-arm_upper_len/2.))
+            .limits([0., 45f32.to_radians()])
+            .motor(0., motor_vel, 1., 0.0);
+        let elb_y_j = RevoluteJointBuilder::new(Vec3::Y)
+            .local_anchor2(Vec3::Y * (arm_lower_len/2.))
+            .limits([0., 180f32.to_radians()])
+            .motor(0., motor_vel, 1., 0.0);
+        let elb_x = commands.spawn(RigidBody::Dynamic).id();
+        let arm_r_lower = make_part!(arm_lower_len)
+            .insert(ImpulseJoint::new(arm_r_upper, no_collision_j))
+            .id();
+        
+        commands.spawn(Arm {
+            chain: vec![
+                (torso, None),
+                (shld_x, Some(shld_x_j.build().data)),
+                (shld_y, Some(shld_y_j.build().data)),
+                (arm_r_upper, Some(shld_z_j.build().data)),
+                (elb_x, Some(elb_x_j.build().data)),
+                (arm_r_lower, Some(elb_y_j.build().data))
+            ]
+        });
+
+    }
+}
+
+#[derive(Component)]
+pub struct Arm {
+    pub chain: Vec<(Entity, Option<GenericJoint>)>
+}
+
+fn chain_arm_joints(
+    mut commands: Commands,
+    mut rapier_ctx: ResMut<RapierContext>,
+    new_arm_q: Query<&Arm, Added<Arm>>
+) {
+    for arm in new_arm_q.iter() {
+        for (i, (body_ent, joint_opt)) in arm.chain.iter().enumerate() {
+            if i == 0 { continue; }
+            let parent_ent = arm.chain.get(i-1).map(|val| val.0);
+            if parent_ent.is_none() || joint_opt.is_none() { continue; }
+            
+            let body1_opt = rapier_ctx.entity2body().get(&parent_ent.unwrap());
+            let body2_opt = rapier_ctx.entity2body().get(body_ent);
+
+            if let (Some(body1), Some(body2)) = (body1_opt, body2_opt) {
+                let body1 = *body1;
+                let body2 = *body2;
+                let body_ent = *body_ent;
+                if let Some(mb_handle) = rapier_ctx.multibody_joints.insert(
+                    body1,
+                    body2, 
+                    joint_opt.unwrap().into_rapier(), 
+                    true
+                ) {
+                    commands.entity(body_ent)
+                        .insert(RapierMultibodyJointHandle(mb_handle));
+                } else {
+                    panic!("Failed to create multibody arm: loop detected.")
+                }
+            }
+        }
     }
 }
